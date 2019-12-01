4d2af4866b9585942aa807fa0be4a74820158384
Marcin <marcin.nieznaj@gmail.com>
2019-12-01 12:56:07 +0100

    test commitowania

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-let quantity = document.getElementById("option-1").getElementsByName("quantity");
-let diameter = document.getElementById("option-1").getElementsByName("diameter");
-let price = document.getElementById("option-1").getElementsByName("price");
+function addNewOption(){
+    let newOptionNo = document.getElementById('offers').getElementsByClassName('option').length;
+    let lastSpot = document.getElementById('offers');
 
-let profitability = ((Math.pow(diameter,2) * Math.PI) * quantity)/price;
+    lastSpot.innerHTML += '<div class="option"> <h2>Opcja '+newOptionNo+'</h2> <div id="option-'+ newOptionNo +'"> <p>Ilość sztuk w zestawie:</p> <input type="text" name="quantity" value=""><br> <p>Średnica:</p> <input type="text" name="diameter" value=""><br> <p>Cena:</p> <input type="text" name="price" value=""><br> </div> <button>Dodaj</button></div>';
+};
 
+function checkOptionsProfitability(option){
+    let quantity = document.getElementById('option-'+option+'-1').value;
+    let diameter = document.getElementById('option-'+option+'-2').value;
+    let price = document.getElementById('option-'+option+'-3').value;
+
+    let profitability = ((Math.pow(diameter,2) * Math.PI) * quantity)/price;
+    return profitability;
+};
+
+
+//TO CO JEST PONIZEJ NARAZIE NIE DZIAŁA LUB NIE JEST UMIESZCZONE W APCE
+
+let profitabilityResults= [];
+
+function checkGeneralProfitability(){
+    let noOfOptions = document.getElementById('offers').getElementsByClassName('option').length-1;
+
+    for(noOfOptions; noOfOptions>0; noOfOptions-1){
+
+        let newResult = [checkOptionsProfitability(noOfOptions),"option-number-"+noOfOptions];
+
+        profitabilityResults = profitabilityResults.push(newResult);
+    };
+    return profitabilityResults;
+    //sprawdzić kolejne pozycje w tabeli i podać największą
+}
