42e949c51c0bb13227bdbea4dc72b3478289e928
Marcin <marcin.nieznaj@gmail.com>
2020-01-01 17:07:35 +0100

    wdraanie BEM

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function addNewOption(){
     let newOptionNo = document.getElementById('offers').getElementsByClassName('option').length;
     let lastSpot = document.getElementById('offers');
 
-    lastSpot.innerHTML += '<div class="option"> <h2>Opcja '+newOptionNo+'</h2> <div id="option-'+ newOptionNo +'"> <p>Ilość sztuk w zestawie:</p> <input type="text" name="quantity" value=""><br> <p>Średnica:</p> <input type="text" name="diameter" value=""><br> <p>Cena:</p> <input type="text" name="price" value=""><br> </div> <button>Dodaj</button></div>';
+    lastSpot.innerHTML += '<div class="option"> <h2>Opcja '+newOptionNo+'</h2> <div id="option-'+ newOptionNo +'"> <p>Ilość sztuk w zestawie:</p> <input class="option__input" type="text" name="quantity" value=""><br> <p>Średnica:</p> <input class="option__input" type="text" name="diameter" value=""><br> <p>Cena:</p> <input class="option__input" type="text" name="price" value=""><br> </div> <button class="button__submit">Dodaj</button></div>';
 };
 
 function checkOptionsProfitability(option){
@@ -23,14 +23,7 @@ let profitabilityResults = [0];
 function checkGeneralProfitability(){
     let noOfOptions = document.getElementById('offers').getElementsByClassName('option').length-1;
     console.log(noOfOptions);
-/*
-    for(let i = noOfOptions; i>0; i-1){
-        let newResult = [checkOptionsProfitability(i),"option-number-"+i];
-        if(profitabilityResults[0] < newResult[0]){
-        profitabilityResults = newResult;
-        };
-    };
-*/
+
     while(noOfOptions > 0){
         let newResult = [checkOptionsProfitability(noOfOptions),"option-number-"+noOfOptions];
         console.log('new result '+newResult);
@@ -43,13 +36,23 @@ function checkGeneralProfitability(){
     console.log(profitabilityResults);
 }
 
+//klasa tworzona za pomocą dodaj w opcji promocji. dodawana do tablicy, w któ®ej jak zgadza się ilość promocji to dopiero mona sprawdzić ilość wyników
 class optionData {
     constructor(quantity, diameter, price){
-        
+        //przygotować sposób tworzenia nazwy
+        this.name = 'option-number-'+ document.getElementsByClassName('option').length;
+        this.quantity = quantity;
+        this.diameter = diameter;
+        this.price = price;
     }
 };
 
+let optionsArray = [];
+
+// czy to ma sens dla wyszukiwania po onclicku reszty danych z parenta dla elementów z klasą options? this.parent.getElementById(this);
+
 function showResults(){
+    checkGeneralProfitability();
     let result = document.getElementById('results');
     result.innerHTML = '<div class="option"><h2>Najlepszą jest opcja nr X</h2><div><p>Ilość sztuk w zestawie:</p><p></p><br><p>Średnica:</p><p></p><br><p>Cena:</p><p></p><br></div><button>Sprawdź ponownie</button></div>';
     result.classList.toggle('hide-element');
